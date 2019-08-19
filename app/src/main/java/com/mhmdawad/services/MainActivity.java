08f31b518be7bde4569596024442416cd4a1ba4d
MhmdAwad <mhmdawaddd@gmail.com>
2019-08-19 19:30:06 +0200

    services

--- app/src/main/java/com/mhmdawad/services/MainActivity.java
+++ app/src/main/java/com/mhmdawad/services/MainActivity.java
@@ -1,54 +1,41 @@
 package com.mhmdawad.services;
 
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+import com.mhmdawad.services.services.HeartIntentService;
+import com.mhmdawad.services.services.ReminderTasks;
 
-    TextView name;
-    TextView points;
+import static com.mhmdawad.services.HeartPreference.HEART_BLACK;
+import static com.mhmdawad.services.HeartPreference.HEART_RED;
+
+public class MainActivity extends AppCompatActivity
+implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    ImageButton heartImageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.name);
-        points = findViewById(R.id.points);
 
-        name.setText(PlayerPreference.getPlayerName(this));
-        points.setText(String.valueOf(PlayerPreference.getPlayerPoints(this)));
+        setupHeartImageButton();
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
-
+        DispatcherUtilities.scheduleNotification(this);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater layoutInflater = getMenuInflater();
-        layoutInflater.inflate(R.menu.settings,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.settings){
-            startActivity(new Intent(MainActivity.this, Settings.class));
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     protected void onDestroy() {
@@ -57,12 +44,49 @@ public class MainActivity extends AppCompatActivity implements
         sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 
+
+    private void setupHeartImageButton(){
+
+        heartImageButton = findViewById(R.id.heart);
+
+        final Intent heartIntent = new Intent(this, HeartIntentService.class);
+        heartIntent.setAction(ReminderTasks.HEART);
+
+        editHeartColor();
+
+        heartImageButton.setOnClickListener(new View.OnClickListener() {
+            Toast toast;
+            @Override
+            public void onClick(View v) {
+                if(toast != null) toast.cancel();
+                if (HeartPreference.getHeartColor(MainActivity.this) == HEART_BLACK) {
+                    heartImageButton.setImageResource(R.drawable.ic_red_heart);
+                    toast = Toast.makeText(MainActivity.this, "I Love u", Toast.LENGTH_SHORT);
+                    HeartNotification.remindUserWithNotification(MainActivity.this);
+                } else if (HeartPreference.getHeartColor(MainActivity.this) == HEART_RED) {
+                    heartImageButton.setImageResource(R.drawable.ic_black_heart);
+                    toast = Toast.makeText(MainActivity.this, "Don't?", Toast.LENGTH_SHORT);
+                }
+                toast.show();
+                startService(heartIntent);
+            }
+        });
+    }
+
+    private void editHeartColor(){
+        switch (HeartPreference.getHeartColor(this)) {
+            case HEART_BLACK:
+                heartImageButton.setImageResource(R.drawable.ic_black_heart);
+                break;
+            case HEART_RED:
+                heartImageButton.setImageResource(R.drawable.ic_red_heart);
+        }
+    }
+
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals("nameForPlayer") ) {
-            name.setText(sharedPreferences.getString(key, ""));
-        }else if( key.equals("pointsForPlayer")){
-            points.setText(String.valueOf(sharedPreferences.getString(key, "")));
+        if(key.equals("heart")){
+            editHeartColor();
         }
     }
 }
